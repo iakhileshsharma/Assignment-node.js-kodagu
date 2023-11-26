d0cc600c008a35cf44e95052b7866cb3ac7a8cd4
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-11-26 16:57:08 +0530

    File added

--- Routes/tasks.js
+++ Routes/tasks.js
@@ -26,4 +26,44 @@ router.post('/', async(req, res)=>{
 });
 
 
+
+
+router.put('/:_id', async (req, res)=>{
+
+    const {error} = validate(req.body)
+    if(error) res.status(400).send(error.details[0].message)
+
+    
+    const task = await Task.findByIdAndUpdate(req.params._id,
+         {title : req.body.title, description : req.body.description},
+         {new : true})
+
+    if(!task) return res.status(404).send('The task with this ID was not found')
+
+    res.send(task);
+});
+
+
+router.delete('/:_id', async (req, res)=> {
+    
+    const task = await Task.findByIdAndDelete(req.params._id)
+
+    if(!task) return res.status(404).send('The task  with the given ID was not found');
+
+    
+    res.send(task);
+});
+
+ 
+router.get('/:_id', async (req, res)=> {
+    const task = await Task.findById(req.params._id)
+    if(!task) return res.status(404).send('The task with the ID was not found');
+
+    res.send(task);
+
+});
+
+
+
+
 module.exports = router
